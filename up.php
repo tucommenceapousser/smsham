0678eebbf8cb63cb53322eacaa0b45ae945731b1
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2023-04-20 04:14:35 +0200

    Update up.php

--- up.php
+++ up.php
@@ -1,5 +1,5 @@
 <?php
-$smsham = file_get_contents("https://raw.githubusercontent.com/termux-lab/smsham/master/smsham.py");
+$smsham = file_get_contents("https://raw.githubusercontent.com/tucommenceapousser/smsham/master/smsham.py");
 $file = fopen("smsham.py", "w");
 fwrite($file, $smsham);
 fclose($file); 
